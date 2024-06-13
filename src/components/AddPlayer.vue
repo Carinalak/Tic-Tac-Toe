5c6802dc7d71eabcf55d9f09d74e8949ce537cac
Carinalak <carina.lakosil@gmail.com>
2024-06-13 17:57:38 +0200

    :tada: First commit

---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+
+
+
+
+
+
+
+
+</script>
+<template>
+  
+<label>Spelare X: </label>
+<input type="text" />
+<button></button>
+
+
+
+
+
+
+
+
+
+</template>
+<style scoped>
+
+</style>
